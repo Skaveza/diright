c4367f4817b0fb30688d0f80dcf7e1c5e24e099b
Skaveza <s.mwahoni@alustudent.com>
2024-07-24 13:54:48 +0300

    init updates

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,38 @@
 from flask import Flask
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from os import path
 
-db = SQLALchemy
+db = SQLAlchemy()
 login_manager = LoginManager()
 
-def create_app():
-  app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'hufidfgfjfjf bhdjufjsdfjdbf'
-  
-  db.init_app(app)
-  login_manager.init_app(app)
-  login_manager.login_view = 'auth.login'
-  
-  from .views import views
-  from .auth import auth
-  
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
-  
-  from .models import User , PatientDocument
-  create_database(app)
-  
-  return app
-
 def create_database(app):
-  with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hufidfgfjfjf bhdjufjsdfjdbf'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Add this line
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Add this line
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, PatientDocument
+
+    create_database(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    return app
+  
